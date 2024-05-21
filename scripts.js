f7415f417353d97bfe451ea05d8ffab10aa18052
RodrigoAndresHG <132313358+RodrigoAndresHG@users.noreply.github.com>
2024-05-21 16:21:29 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,3 @@
-AOS.init();
-
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
@@ -7,26 +5,4 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
             behavior: 'smooth'
         });
     });
-});
-
-var swiper = new Swiper('.swiper-container', {
-    loop: true,
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
-});
-
-document.getElementById('language-select').addEventListener('change', function () {
-    var selectedLanguage = this.value;
-    var currentUrl = window.location.href;
-    if (selectedLanguage === 'en') {
-        window.location.href = currentUrl.replace('/es', '/en');
-    } else if (selectedLanguage === 'es') {
-        window.location.href = currentUrl.replace('/en', '/es');
-    }
 });
